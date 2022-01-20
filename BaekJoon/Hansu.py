6e95e37a4b5200a4b6c526dec97c4145130290e0
yoon <seoung59@gmail.com>
2022-01-20 15:27:26 +0900

    algorithm

---
+++ BaekJoon/Hansu.py
@@ -0,0 +1,22 @@
+def checkNum(x):
+    a = [0 for _ in range(10)]
+    idx = 0
+
+    while(x):
+        a[idx] = x % 10
+        x//=10
+        idx += 1
+
+    for i in range(idx-2):
+        if(a[i] - a[i+1] != a[i+1] - a[i+2]): return False
+    return True
+
+n = int(input())
+cnt = 0
+for num in range(1,n+1):
+    if(checkNum(num)): 
+        cnt+=1
+    else:
+        continue
+
+print(cnt)
