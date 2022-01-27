de2f49c8f757cd6060c610b675785b7642e94faf
yoon <seoung59@gmail.com>
2022-01-27 21:49:48 +0900

    algoritm

--- Greedy/greedy05.py
+++ Greedy/greedy05.py
@@ -1,3 +1,17 @@
-visit = {}
-
-visit.update
+def solution(n, costs):
+    ans = 0
+    costs.sort(key = lambda x: x[2]) # cost 기준으로 오름차순 정렬
+    #[[0,1,1],[1,3,1],[0,2,2],[1,2,5],[2,3,8]]
+    #costs[0],costs[1],costs[2] = 0 1 1 -> (0,1)
+    routes = set({costs[0][0],costs[0][1]})
+    ans+=costs[0][2]
+    costs.pop(0)
+    while len(routes)!=n:
+        for cost in (costs):
+            if cost[0] in routes and cost[1] in routes:
+                continue
+            if cost[0] in routes or cost[1] in routes:
+                routes.update([cost[0], cost[1]])
+                ans += cost[2]
+                break
+    return ans
--- Greedy/greedy06.py
+++ Greedy/greedy06.py
@@ -3,7 +3,14 @@ def solution(routes):
     routes.sort(key = lambda x:x[0])
     answer = 1
     end = routes[0][1]
-    
+    [-----------------------]
+    [-----]
+        [--------]
+          c<-카메라 설치
+          [-----------]
+                 c<-카메라 설치
+                         [--]
+                            c<-카메라 설치
     for i in range(1,len(routes)):
         if routes[i][0] <= end:
             end = min(routes[i][1], end)
